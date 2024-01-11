1f54e6c14eaed382e0856ab416e0343ee2f2ad88
Anton Kashnikov <anton.kashnikoff@icloud.com>
2024-01-11 18:18:24 +0100

    Refactored code

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -26,35 +26,25 @@ final class ActionViewController: UIViewController {
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             // Input item contains an array of attachments, which are given to us wrapped up as an NSItemProvider. Our code pulls out the first attachment from the first input item.
             if let itemProvider = inputItem.attachments?.first {
-                if #available(iOSApplicationExtension 14.0, *) {
-                    // Ask the item provider to actually provide us with its item.
-                    // The method will carry on executing while the item provider is busy loading and sending us its data.
-                    itemProvider.loadItem(forTypeIdentifier: UTType.propertyList.identifier as String) { [weak self] dict, error in
-                        guard
-                            let itemDictionary = dict as? NSDictionary,
-                            let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary
-                        else { return }
-                        
-                        self?.pageTitle = javaScriptValues["title"] as? String ?? ""
-                        self?.pageURL = javaScriptValues["URL"] as? String ?? ""
-                        
-                        DispatchQueue.main.async {
-                            self?.title = self?.pageTitle
-                        }
-                    }
+                let identifier = if #available(iOSApplicationExtension 14.0, *) {
+                    UTType.propertyList.identifier as String
                 } else {
-                    itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] dict, error in
-                        guard
-                            let itemDictionary = dict as? NSDictionary,
-                            let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary
-                        else { return }
-                        
-                        self?.pageTitle = javaScriptValues["title"] as? String ?? ""
-                        self?.pageURL = javaScriptValues["URL"] as? String ?? ""
-                        
-                        DispatchQueue.main.async {
-                            self?.title = self?.pageTitle
-                        }
+                    kUTTypePropertyList as String
+                }
+                
+                // Ask the item provider to actually provide us with its item.
+                // The method will carry on executing while the item provider is busy loading and sending us its data.
+                itemProvider.loadItem(forTypeIdentifier: identifier) { [weak self] dict, error in
+                    guard
+                        let itemDictionary = dict as? NSDictionary,
+                        let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary
+                    else { return }
+                    
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
                     }
                 }
             }
